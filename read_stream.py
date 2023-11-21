370711ed1ae0263679492172e456d142eeb17b96
PhillipHenry <PhillHenry@gmail.com>
2023-11-21 14:50:37 +0000

    Goddamit, this works!

--- read_stream.py
+++ read_stream.py
@@ -5,18 +5,14 @@ import pyarrow as pa
 filename = '/dev/shm/arrow'
 
 # Create an Arrow InputStream from the stream
-with pa.input_stream(filename) as stream:
-    input_stream = stream.read()
-    reader = pa.ipc.open_stream(input_stream)
-    record_batch = reader.read_all()
-    df = record_batch.to_pandas()
-    print(df)
+def read_from_shm():
+    with pa.input_stream(filename) as stream:
+        input_stream = stream.read()
+        reader = pa.ipc.open_stream(input_stream)
+        schema = reader.schema
+        record_batch = reader.read_next_batch()
+        print(record_batch)
 
-    # Read the Arrow stream
-    record_batch = pa.ipc.read_record_batch(input_stream)
 
-    # Convert the RecordBatch to a Pandas DataFrame
-    df = record_batch.to_pandas()
-
-    # Print the DataFrame
-    print(df)
+if __name__ == "__main__":
+    read_from_shm()
